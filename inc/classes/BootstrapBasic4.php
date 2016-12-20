3ab1678848eebc3fd22d54169e2d84c7e7eb4061
vee w <kawewong@gmail.com>
2016-12-20 18:44:27 +0700

    update tether and typo fix

--- inc/classes/BootstrapBasic4.php
+++ inc/classes/BootstrapBasic4.php
@@ -68,13 +68,13 @@ if (!class_exists('\\BootstrapBasic4\\BootstrapBasic4')) {
             wp_enqueue_style('bootstrap-basic4-wp-main', get_stylesheet_uri());
 
             wp_enqueue_style('bootstrap4', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '4.0.0-a5');
-            wp_enqueue_style('font-awesome4', get_template_directory_uri() . '/assets/css/font-awesome.min.css', array(), '4.6.3');
+            wp_enqueue_style('font-awesome4', get_template_directory_uri() . '/assets/css/font-awesome.min.css', array(), '4.7.0');
             wp_enqueue_style('bootstrap-basic4-main', get_template_directory_uri() . '/assets/css/main.css');
 
             if (is_singular() && get_option('thread_comments')) {
                 wp_enqueue_script('comment-reply');
             }
-            wp_enqueue_script('bootstrap-basic4-tether', get_template_directory_uri() . '/assets/js/tether.min.js', array(), '1.3.1', true);// required for Bootstrap tooltip.
+            wp_enqueue_script('bootstrap-basic4-tether', get_template_directory_uri() . '/assets/js/tether.min.js', array(), '1.3.3', true);// required for Bootstrap tooltip.
             wp_enqueue_script('bootstrap4', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '4.0.0-a5', true);
         }// enqueueScriptsAndStyles
 
