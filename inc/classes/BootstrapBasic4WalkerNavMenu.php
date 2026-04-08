aa118bd48a0341cd1702215e5ff14c6937f1760e
ve3 <kawewong@gmail.com>
2026-04-09 00:33:19 +0700

    Fix incorrect escape URL.

--- inc/classes/BootstrapBasic4WalkerNavMenu.php
+++ inc/classes/BootstrapBasic4WalkerNavMenu.php
@@ -154,7 +154,7 @@ if (!class_exists('\\BootstrapBasic4\\BootstrapBasic4WalkerNavMenu')) {
                 $attributes = !empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
                 $attributes .=!empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
                 $attributes .=!empty($item->xfn) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
-                $attributes .=!empty($item->url) ? ' href="' . esc_attr($item->url) . '"' : '';
+                $attributes .=!empty($item->url) ? ' href="' . esc_url($item->url) . '"' : '';
                 $a_class_names = '';
                 if (isset($item->classes) && is_array($item->classes)) {
                     $a_class_names = join(' ', $item->classes);
